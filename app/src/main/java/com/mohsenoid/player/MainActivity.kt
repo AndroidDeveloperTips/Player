efe2842f5d1c754516580a1f56f8f39dd880d214
Mohsen <msn.mirhoseini@gmail.com>
2023-01-09 13:23:58 +0100

    Implement Picture in Picture feature

--- app/src/main/java/com/mohsenoid/player/MainActivity.kt
+++ app/src/main/java/com/mohsenoid/player/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.mohsenoid.player
 
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.graphics.Rect
+import android.graphics.drawable.Icon
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.mohsenoid.player.databinding.ActivityMainBinding
@@ -11,6 +22,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val isPipSupported by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+        } else false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,6 +35,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         setupPlayer()
+        setupPipButton()
+        setupAutoPip()
     }
 
     private fun setupPlayer() {
@@ -37,10 +56,82 @@ class MainActivity : AppCompatActivity() {
         binding.title.setText(R.string.dcbln22)
     }
 
+    private fun setupPipButton() {
+        binding.button.setOnClickListener {
+            if (!isPipSupported) return@setOnClickListener
+
+            val params = getPipParams() ?: return@setOnClickListener
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                enterPictureInPictureMode(params)
+            }
+        }
+    }
+
+    private fun setupAutoPip() {
+        val params = getPipParams() ?: return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setPictureInPictureParams(params)
+        }
+    }
+
+    private fun getPipParams(): PictureInPictureParams? {
+        val rect = Rect()
+        binding.player.getGlobalVisibleRect(rect)
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val builder = PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(16, 9))
+                .setSourceRectHint(rect)
+                .setActions(
+                    listOfNotNull(
+                        getPipAction()
+                    )
+                )
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                builder.setAutoEnterEnabled(true)
+            }
+
+            builder.build()
+        } else null
+    }
+
+    private fun getPipAction(): RemoteAction? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val actionUri = Uri.parse("https://youtube.com/AndroidDeveloperTips")
+            val actionIntent = Intent(Intent.ACTION_VIEW, actionUri)
+            val actionPendingIntent = PendingIntent.getActivity(this, 0, actionIntent, PendingIntent.FLAG_IMMUTABLE)
+            val remoteAction = RemoteAction(
+                Icon.createWithResource(this, R.drawable.ic_picture_in_picture_action),
+                "More info",
+                "More info action",
+                actionPendingIntent
+            )
+            remoteAction
+        } else null
+    }
+
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        }
+        if (isInPictureInPictureMode) {
+            binding.title.isVisible = false
+            binding.button.isVisible = false
+        } else {
+            binding.title.isVisible = true
+            binding.button.isVisible = true
+        }
+    }
 
     override fun onPause() {
         super.onPause()
-        binding.player.player?.pause()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            if (!isInPictureInPictureMode) {
+                binding.player.player?.pause()
+            }
+        }
     }
 
     override fun onResume() {
