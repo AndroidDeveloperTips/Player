a2de2346e5f5510acfd0e73c9a316dd36eff600f
Mohsen <msn.mirhoseini@gmail.com>
2023-01-09 13:19:53 +0100

    Implement ExoPlayer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.exoplayer:exoplayer:2.18.2")
 
     testImplementation("junit:junit:4.13.2")
 
--- app/src/main/java/com/mohsenoid/player/MainActivity.kt
+++ app/src/main/java/com/mohsenoid/player/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mohsenoid.player
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.mohsenoid.player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setupPlayer()
+    }
+
+    private fun setupPlayer() {
+        val videoUri: Uri = Uri.parse("android.resource://" + packageName.toString() + "/" + R.raw.dcbln22)
+        val mediaItem: MediaItem = MediaItem.fromUri(videoUri)
+
+        val exoPlayer = ExoPlayer.Builder(this).build().apply {
+            setMediaItem(mediaItem)
+            prepare()
+            play()
+            repeatMode = ExoPlayer.REPEAT_MODE_ONE
+        }
+
+        binding.player.player = exoPlayer
+        binding.player.useController = false
+
+        binding.title.setText(R.string.dcbln22)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        binding.player.player?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.player.player?.play()
     }
 }
